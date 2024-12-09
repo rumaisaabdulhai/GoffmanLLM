20350a0dd067a3f93d4e8c512a27fc6fc5b4ff38
Rumaisa Abdulhai <rumaisa.abdulhai@gmail.com>
2024-12-09 12:42:36 -0500

    revert prompt

--- prompts.py
+++ prompts.py
@@ -1,11 +1,6 @@
-# def top_topics_prompt(audience, topics_list, num_topics):
-#   return f"You are at a dinner with {audience}. Here is a list of topics you could discuss with them: {topics_list}. \
-#   Please select the top {num_topics} topics you would most likely discuss and only report the names of the topics. Example of your answer would look like: Topic 1, Topic 2, Topic 3"
-
 def top_topics_prompt(audience, topics_list, num_topics):
   return f"You are at a dinner with {audience}. Here is a list of topics you could discuss with them: {topics_list}. \
-  Please select the top {num_topics} topics you would most likely discuss and only report the topic names. Example response: Topic 1, Topic 2, Topic 3"
-
+  Please select the top {num_topics} topics you would most likely discuss and only report the names of the topics. Example of your answer would look like: Topic 1, Topic 2, Topic 3"
 
 def yes_or_no_topic_prompt(audience, topic):
   return f"You are at a dinner with {audience}. Would you discuss this topic with them: {topic}? Report 1 if you would talk about it or 0 if you would not talk about it. Please just output the number."
